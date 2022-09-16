561d23a95c97c08ac822935eba043f6f87a194fe
bobyeong2 <bobyeong2@naver.com>
2022-09-17 08:42:39 +0900

    Feat : create templates folder & account view
    account app에서 보여질 template 파일 추가

--- accountapp/views.py
+++ accountapp/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def hello_world(request):
-    return HttpResponse("hello world")
+    return render(request, 'accountapp/helloworld.html')
