996e5710c6d72970122d66293c102c536a43e952
bobyeong2 <bobyeong2@naver.com>
2022-09-17 13:36:29 +0900

    Feat : account login / logout add
    account Login, Logout 기능 구현

--- postwebapp/settings.py
+++ postwebapp/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import environ
 import os
+from django.urls import reverse_lazy
 
 env = environ.Env(
     # set casting, default value
@@ -138,6 +139,9 @@ STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+LOGIN_REDIRECT_URL = reverse_lazy('accountapp:hello_world')
+LOGOUT_REDIRECT_URL = reverse_lazy('accountapp:login')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
