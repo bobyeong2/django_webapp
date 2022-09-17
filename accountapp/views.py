f5001e16f98b6a1821010d680892fdb79066ed0f
bobyeong2 <bobyeong2@naver.com>
2022-09-17 15:32:47 +0900

    Feat : user authentication add
    사용자 인증 기능 구현
    사용자가 로그인 / 로그아웃 시 다른사람의 update,delete Page가 접속되어 접속되지 않게 인증기능 decorator 구현

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,26 +1,33 @@
+from http.client import FORBIDDEN
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect,HttpResponseForbidden
 from django.urls import reverse, reverse_lazy
 from accountapp.models import HelloWorld
 from django.views.generic import CreateView,DetailView,UpdateView,DeleteView
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 from accountapp.forms import AccountUpdateForm
+from accountapp.decorators import account_ownership_required
 # Create your views here.
 
+has_ownership = [account_ownership_required,login_required]
+
+@login_required
 def hello_world(request):
 
     if request.method == "POST":
 
-        temp = request.POST.get('hello_world_input')
-        new_hello_world = HelloWorld()
-        new_hello_world.text = temp
-        hello_world_list = HelloWorld.objects.all()
-        new_hello_world.save()
+            temp = request.POST.get('hello_world_input')
+            new_hello_world = HelloWorld()
+            new_hello_world.text = temp
+            hello_world_list = HelloWorld.objects.all()
+            new_hello_world.save()
 
 
-        return HttpResponseRedirect(reverse('accountapp:hello_world'))
+            return HttpResponseRedirect(reverse('accountapp:hello_world'))
 
     else :
         hello_world_list = HelloWorld.objects.all()
@@ -38,6 +45,8 @@ class AccountDetilView(DetailView):
     context_object_name = "target_user"
     template_name = 'accountapp/detail.html'
 
+@method_decorator(has_ownership,'get')
+@method_decorator(has_ownership,'post')
 class AccountUpdateView(UpdateView):
     model = User
     context_object_name = "target_user"
@@ -45,6 +54,8 @@ class AccountUpdateView(UpdateView):
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/update.html'
 
+@method_decorator(has_ownership,'get')
+@method_decorator(has_ownership,'post')
 class AccountDeleteView(DeleteView):
     model = User
     context_object_name = "target_user"
