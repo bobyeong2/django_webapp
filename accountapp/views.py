09dddcf1321d95fb8411c3417d8e8f982ca0ef56
bobyeong2 <bobyeong2@naver.com>
2022-10-17 21:39:46 +0900

    Fixed Design : account Details
    계정 상세정보 내 디자인 수정 -> google materials=-icon을 사용

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse, reverse_lazy
-from accountapp.models import HelloWorld
 from django.views.generic import CreateView,DetailView,UpdateView,DeleteView
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
@@ -16,29 +15,10 @@ from articleapp.models import Article
 
 has_ownership = [account_ownership_required,login_required]
 
-@login_required
-def hello_world(request):
-
-    if request.method == "POST":
-
-            temp = request.POST.get('hello_world_input')
-            new_hello_world = HelloWorld()
-            new_hello_world.text = temp
-            hello_world_list = HelloWorld.objects.all()
-            new_hello_world.save()
-
-
-            return HttpResponseRedirect(reverse('accountapp:hello_world'))
-
-    else :
-        hello_world_list = HelloWorld.objects.all()
-        return render(request, 'accountapp/helloworld.html',context={'hello_world_list':hello_world_list})
-
-
 class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
+    success_url = reverse_lazy('home')
     template_name = 'accountapp/create.html'
 
 class AccountDetilView(DetailView,MultipleObjectMixin):
@@ -58,7 +38,7 @@ class AccountUpdateView(UpdateView):
     model = User
     context_object_name = "target_user"
     form_class = AccountUpdateForm
-    success_url = reverse_lazy('accountapp:hello_world')
+    success_url = reverse_lazy('home')
     template_name = 'accountapp/update.html'
 
 @method_decorator(has_ownership,'get')
@@ -66,5 +46,5 @@ class AccountUpdateView(UpdateView):
 class AccountDeleteView(DeleteView):
     model = User
     context_object_name = "target_user"
-    success_url = reverse_lazy('accountapp:login')
+    success_url = reverse_lazy('home')
     template_name = 'accountapp/delete.html'
